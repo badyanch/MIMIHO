57e913b287b19098b3bd8526d17ed9feb22e754d
BratytsiaDmytro <bratytsiaDmytro@gmail.com>
2023-01-29 16:51:24 +0200

    add translate picture

--- src/js/slider.con.js
+++ src/js/slider.con.js
@@ -14,11 +14,11 @@ function swiperSlider() {
       let swiper = new Swiper(slider.querySelector('.swiper'), {
         speed: 1500,
         // автоплей
-        // centeredSlides: false,
-        // autoplay: {
-        // delay: 3000,
-        //     disableOnInteraction: false,
-        // },
+        centeredSlides: false,
+        autoplay: {
+        delay: 3000,
+            disableOnInteraction: false,
+        },
         slidesPerView: 2, // кількість слайдерів для показу
         spaceBetween: 16, // відстань між слайдерами
 
