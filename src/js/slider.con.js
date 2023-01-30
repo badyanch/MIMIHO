487ba1d02016c2808255dadbc53e379623a4e2c7
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-01-30 23:58:23 +0200

    create slider and fix background sizes

--- src/js/slider.con.js
+++ src/js/slider.con.js
@@ -310,5 +310,86 @@ const aboutHotel = document.querySelectorAll('[data-slider="hotel-slider"]');
     });
   }
 
+   const restaurantAbout = document.querySelectorAll('[data-slider="restaurantAbout"]');
+
+  
+  //const heroSlider = document.querySelectorAll('[data-slider="hero-slider"]');
+  if (restaurantAbout) {
+    restaurantAbout.forEach(slider => {
+      // кнопки вперед та назад
+      let arrowLeft = slider.querySelector('.swiper-button-prev');
+      let arrowRight = slider.querySelector('.swiper-button-next');
+
+      //коментуємо чи видаляемо якщо не потрібно
+      let pagination = slider.querySelector('.swiper-pagination');
+      //коментуємо чи видаляемо якщо не потрібно
+
+      let swiper = new Swiper(slider.querySelector('.restaurant-about__swiper'), {
+        speed: 1500,
+        // автоплей
+        // centeredSlides: false,
+        // autoplay: {
+        // delay: 3000,
+        //     disableOnInteraction: false,
+        // },
+        slidesPerView: 2, // кількість слайдерів для показу
+        spaceBetween: 16, // відстань між слайдерами
+
+        // крапки пагінації
+        pagination: {
+          el: pagination,
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<li class="' + className + '"></li>';
+          },
+        },
+        // кнопки навігації
+        navigation: {
+          nextEl: arrowRight,
+          prevEl: arrowLeft,
+        },
+
+        // додаємо додатковий клас
+        // можна використовувати для додаткових анімацій
+        on: {
+          transitionStart: function () {
+            let previousIndex = this.previousIndex;
+            let previousSlide =
+              slider.getElementsByClassName('restaurant-about__swiper-inner')[previousIndex];
+            if (previousSlide) {
+              setTimeout(function () {
+                previousSlide.classList.remove('is-play');
+              }, 1000);
+            }
+          },
+          transitionEnd: function () {
+            let activeIndex = this.activeIndex;
+            let activeSlide =
+              slider.getElementsByClassName('restaurant-about__swiper-inner')[activeIndex];
+            activeSlide.classList.add('is-play');
+          },
+        },
+        // адаптив
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView:2,
+            spaceBetween: 16,
+          },
+          // when window width is >= 768px
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          // when window width is >= 1200px
+          1280: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+        },
+      });
+    });
+  }
+
 }
 window.addEventListener('load', swiperSlider, false);
